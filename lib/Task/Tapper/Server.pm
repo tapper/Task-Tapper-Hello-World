f95aef0348c91e50963d8daafb33ddb9704bf899
Steffen Schwigon <ss5@renormalist.net>
2012-11-14 14:50:03 +0100

    dependencies for an easy "hello world"-style start

---
+++ lib/Task/Tapper/Server.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package Task::Tapper::Hello::World;
+# ABSTRACT: Tapper - The easiest start without hassle (hopefully)
+
+
+=pkgroup Explicit troublemaker deps
+
+
+=pkg File::Slurp
+
+=pkg File::Copy::Recursive
+
+=pkg Test::WWW::Mechanize
+
+=pkg IO::Interactive
+
+=pkg DBI
+
+=pkg DBD::SQLite
+
+=pkg Template::Plugin::Autoformat
+
+=pkg Module::Install::Catalyst
+
+
+
+=pkgroup Tapper
+
+
+=pkg Tapper::Config
+
+=pkg Tapper::CLI
+
+=pkg Tapper::TAP::Harness
+
+=pkg Tapper::Reports::Receiver
+
+=pkg Tapper::Reports::API
+
+=pkg Tapper::Reports::Web
+
+
+1;
